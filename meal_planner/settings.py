73501312d598f2780bbf132991fbd7e4b85decc2
David D'Annunzio <david.dannunzio@gmail.com>
2016-04-01 22:39:55 -0400

    Try again

--- meal_planner/settings.py
+++ meal_planner/settings.py
@@ -67,8 +67,9 @@ WSGI_APPLICATION = 'meal_planner.wsgi.application'
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
-DATABASES['default'] =  dj_database_url.config()
-
+DATABASES = {
+     'default' = dj_database_url.config()
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
